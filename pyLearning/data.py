2ef7ce1e424098e73e0b4dc04ce9a68f1d4fd959
Felix chinonso Emmanuel <Feelchi1star@gmail.com>
2023-06-18 16:54:24 +0100

    updated

---
+++ pyLearning/data.py
@@ -0,0 +1,27 @@
+data = [
+    {"_id": "AdVXiA8jCiT", "content": "Reality leaves a lot to the imagination.", "author": "John Lennon", "tags": [
+        "Famous Quotes"], "authorSlug":"john-lennon", "length":40, "dateAdded":"2020-12-06", "dateModified":"2023-04-14"},
+
+    {"_id": "iUBBoBk8rSal", "content": "If you want your life to be more rewarding, you have to change the way you think.", "author": "Oprah Winfrey",
+        "tags": ["Famous Quotes", "Change"], "authorSlug":"oprah-winfrey", "length":81, "dateAdded":"2021-01-11", "dateModified":"2023-04-14"},
+
+    {"_id": "sCR2ucyf1idj", "content": "The energy of the mind is the essence of life.", "author": "Aristotle", "tags": [
+        "Famous Quotes"], "authorSlug":"aristotle", "length":46, "dateAdded":"2019-09-19", "dateModified":"2023-04-14"},
+    {"_id": "3QpzvAuTw-", "content": "Love, friendship and respect do not unite people as much as a common hatred for something.",
+        "author": "Anton Chekhov", "tags": ["Friendship"], "authorSlug":"anton-chekhov", "length":90, "dateAdded":"2020-03-27", "dateModified":"2023-04-14"},
+    {"_id": "r2gCpEgG_LP", "content": "And I love that even in the toughest moments, when we're all sweating it - when we're worried that the bill won't pass, and it seems like all is lost - Barack never lets himself get distracted by the chatter and the noise. Just like his grandmother, he just keeps getting up and moving forward... with patience and wisdom, and courage and grace.",
+        "author": "Michelle Obama", "tags": ["Wisdom"], "authorSlug":"michelle-obama", "length":345, "dateAdded":"2019-08-03", "dateModified":"2023-04-14"}, {"_id": "-1B7G54OXj", "content": "Wisdom is a kind of knowledge. It is knowledge of the nature, career, and consequences of human values.", "author": "Sidney Hook", "tags": ["Wisdom"], "authorSlug":"sidney-hook", "length":103, "dateAdded":"2020-01-27", "dateModified":"2023-04-14"},
+    {"_id": "xdmXfzn20ml", "content": "The path to success is to take massive, determined action.", "author": "Tony Robbins", "tags": [
+        "Success", "Famous Quotes"], "authorSlug":"tony-robbins", "length":58, "dateAdded":"2019-07-23", "dateModified":"2023-04-14"},
+
+    {"_id": "icfoRWBmqAa", "content": "Faith in oneself is the best and safest course.", "author": "Michelangelo", "tags": [
+        "Famous Quotes"], "authorSlug":"michelangelo", "length":47, "dateAdded":"2020-11-13", "dateModified":"2023-04-14"},
+    {"_id": "PCb9mzq4_Q_w", "content": "The ladder of success is never crowded at the top.", "author": "Napoleon Hill", "tags": [
+            "Success"], "authorSlug":"napoleon-hill", "length":50, "dateAdded":"2019-10-18", "dateModified":"2023-04-14"},
+
+    {"_id": "PCb9mzq4_Q_w", "content": "The ladder of success is never crowded at the top.", "author": "Napoleon Hill",
+        "tags": ["Success"], "authorSlug":"napoleon-hill", "length":50, "dateAdded":"2019-10-18", "dateModified":"2023-04-14"},
+    {"_id": "K67NZgh4Zr0", "author": "The Buddha", "content": "Speak only endearing speech, speech that is welcomed. Speech, when it brings no evil to others, is a pleasant thing.",
+        "tags": ["Wisdom", "Virtue", "Honor"], "authorSlug":"the-buddha", "length":116, "dateAdded":"2023-03-30", "dateModified":"2023-04-14"}
+
+]
---
+++ pyLearning/js.py
@@ -0,0 +1,17 @@
+import json
+data = '''
+  [
+    { "id" : "001",
+      "x" : "2",
+     "name" : "Quincy"
+    } ,
+    { "id" : "009",
+      "x" : "7",
+      "name" : "Mrugesh"
+    }
+  ]
+'''
+info = json.loads(data)
+# if __name__ == "__main__":
+#     print(info[1]['name'])
+print(__name__)
